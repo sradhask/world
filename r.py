531f4800b9610d439d5630edcee196f6cfb2b6bc
sradha <sradhask345@gmail.com>
2024-07-12 08:58:43 +0530

    done

---
+++ r.py
@@ -0,0 +1,10 @@
+a=int(input("enter number of elements"))
+print("enter the elements")
+b=[]
+for i in range(a):
+       c=int(input())
+       b.append(c)
+target=int(input("enter the target"))
+if(c==target):
+  print(c)
+       
---
+++ y.py
@@ -0,0 +1,4 @@
+for i in range(1,6):
+    for k in range (i):
+        print(k,end="")
+    print()

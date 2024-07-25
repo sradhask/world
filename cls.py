c26caee06bc2c9acd4a6639e24a5deb543831c56
sradha <sradhask345@gmail.com>
2024-07-25 08:55:10 +0530

    done

---
+++ cls.py
@@ -0,0 +1,11 @@
+class House:
+    squarefeet=100
+    color="white"
+    cost=2000000
+    def sale(self):
+        print("for sale")
+        print("s.sqarefeet")
+        print("s.color")
+        print("s.cost")
+p=House
+p.sale("sradha")
